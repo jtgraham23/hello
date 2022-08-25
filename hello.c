9b8a7ae154b54798926d48cc946e950a97420465
jtgraham23 <jgraham34@huskers.unl.edugit config --global user.name Jaterrius>
2022-08-25 19:55:44 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: JT Graham
+ * Date: 2022/23/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
